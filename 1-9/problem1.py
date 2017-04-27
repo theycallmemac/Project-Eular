605426ae139c5a32cba60cf872bb7279d78d868a
James Mc Dermott <james.mcdermott89@gmail.com>
2017-04-27 14:56:26 +0100

    Create problem1.py

---
+++ 1-9/problem1.py
@@ -0,0 +1,24 @@
+lst = []
+for i in range(1000):
+	lst.append(i)
+	
+def sum_of_multiples(lst):
+	mults = []
+	for i in lst:
+		if i % 3 == 0:
+			mults.append(i)
+		elif i % 5 == 0: 
+			mults.append(i)
+		else:
+			pass
+			
+	sum = 0
+	for i in mults:
+	   sum = sum + i
+	return sum
+
+def main():
+	print(sum_of_multiples(lst))
+
+if __name__ == '__main__':
+main()
